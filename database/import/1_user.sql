51187923b321735c74e7d8057df93beafffe90f0
Mafciejewicz <melonik0.9846@gmail.com>
2020-06-18 02:41:18 +0200

    Added application password to schema

--- database/import/1_user.sql
+++ database/import/1_user.sql
@@ -1,3 +1,4 @@
-create user if not exists application;
 CREATE DATABASE if not exists szkola character set UTF8MB4 collate utf8mb4_bin;
+
+create user if not exists 'application' identified with mysql_native_password by '7966e4d8-cb1b-548a-859b-b8e336f9fcae';
 GRANT SELECT, UPDATE, INSERT, DELETE ON szkola.* TO application;
